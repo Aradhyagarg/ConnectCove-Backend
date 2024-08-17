fa939e84997c43b0af4b49f0f81f1f310f170842
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2024-08-17 21:04:11 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
+app.use(cors());
 
 if(process.env.NODE_ENV !== "production"){
     require("dotenv").config();
